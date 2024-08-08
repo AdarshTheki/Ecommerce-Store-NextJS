fb197f317e61fdc834bd787192bd973486c5bd02
Adarsh <adarshverma549@gmail.com>
2024-08-08 21:17:32 +0530

    modify miner bug

--- app/(root)/products/[productId]/page.tsx
+++ app/(root)/products/[productId]/page.tsx
@@ -1,59 +1,39 @@
 import ProductInfo from '@/components/ProductInfo';
 import Gallery from '@/components/Gallery';
 import ProductCard from '@/components/ProductCard';
-// import ReviewCard from '@/components/ReviewCard';
-
-export async function ProductGallery({ id }: { id: string }) {
-    const product: ProductType = await (
-        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${id}`)
-    ).json();
-
-    return product?._id ? (
-        <div className='flex justify-center items-start gap-16 py-10 max-md:flex-col max-md:items-center'>
-            <Gallery productMedia={product.media} />
-            <ProductInfo productInfo={product} />
-        </div>
-    ) : (
-        <h2 className='text-center py-10 text-base-medium'>Loading data | Please wait...</h2>
-    );
-}
-
-export async function ProductRelated({ id }: { id: string }) {
-    const related: ProductType[] = await (
-        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${id}/related`)
-    ).json();
-
-    return (
-        <div className='w-full'>
-            <p className='text-heading3-bold text-center py-5'>Related Products</p>
-            {related?.length > 0 ? (
-                <div className='grid grid-cols-2 sm:grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-5 md:gap-4'>
-                    {related.map((product) => (
-                        <ProductCard key={product._id} product={product} />
-                    ))}
-                </div>
-            ) : (
-                <h2 className='text-center py-10 text-base-medium'>
-                    Loading data | Please wait...
-                </h2>
-            )}
-        </div>
-    );
-}
 
 export default async function Page({ params: { productId } }: { params: { productId: string } }) {
+    const product = await (
+        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`)
+    ).json();
+
+    const related = await (
+        await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/related`)
+    ).json();
+
     return (
         <div className='px-4 mx-auto max-w-[1000px]'>
-            <ProductGallery id={productId} />
-            <ProductRelated id={productId} />
+            {/*  */}
+            <div className='flex justify-center items-start gap-16 py-10 max-md:flex-col max-md:items-center'>
+                <Gallery productMedia={product?.media} />
+                <ProductInfo productInfo={product} />
+            </div>
+
+            {/*  */}
+            <div className='w-full'>
+                <p className='text-heading3-bold text-center py-5'>Related Products</p>
+                {related?.length > 0 ? (
+                    <div className='grid grid-cols-2 sm:grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-5 md:gap-4'>
+                        {related?.map((product: ProductType) => (
+                            <ProductCard key={product._id} product={product} />
+                        ))}
+                    </div>
+                ) : (
+                    <h2 className='text-center py-10 text-base-medium'>
+                        Loading data | Please wait...
+                    </h2>
+                )}
+            </div>
         </div>
     );
 }
-
-// const productRes = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${productId}`);
-
-// const reviewsRes = await fetch(`/api/users/review/${productId}`);
-
-// const relatedRes = await fetch(
-//     `${process.env.NEXT_PUBLIC_API_URL}/products/${productId}/related`
-// );
--- app/(root)/products/page.tsx
+++ app/(root)/products/page.tsx
@@ -1,30 +1,22 @@
 import ProductList from '@/components/ProductList';
 import { getProducts } from '@/lib/actions';
-import { Suspense } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
-export const loading = async () => {
-    return (
-        <div className='grid grid-cols-2 px-3 py-10 mx-auto max-w-[1000px] sm:grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-5 md:gap-4'>
-            {Array.from({ length: 20 }, (_, index) => (
-                <div className='space-y-2 rounded-lg overflow-hidden' key={index}>
-                    <Skeleton height={200} />
-                    <Skeleton height={50} />
-                </div>
-            ))}
-        </div>
-    );
-};
-
 export default async function Page() {
-    try {
-        const product = await getProducts(1, 100);
+    const product = await getProducts(1, 100);
+
+    if (!product) {
         return (
-            <Suspense fallback={loading()}>
-                <ProductList productData={product?.products} />
-            </Suspense>
+            <div className='grid grid-cols-2 px-3 py-10 mx-auto max-w-[1000px] sm:grid-cols-3 gap-2 md:grid-cols-4 lg:grid-cols-5 md:gap-4'>
+                {Array.from({ length: 20 }, (_, index) => (
+                    <div className='space-y-2 rounded-lg overflow-hidden' key={index}>
+                        <Skeleton height={200} />
+                        <Skeleton height={50} />
+                    </div>
+                ))}
+            </div>
         );
-    } catch (err) {
-        return <>{loading()}</>;
     }
+
+    return <ProductList productData={product?.products} />;
 }
